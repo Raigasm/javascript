078154297d4540112ebad4adae6e7ae3df710f71
Rai <rai@rbutera.com>
2017-10-16 23:29:57 +0100

    added CLT functionality

--- triangle.js
+++ triangle.js
@@ -167,3 +167,15 @@ Triangle.interpret = function(a, b, c) {
 };
 
 module.exports = Triangle;
+
+
+if (process.argv.length > 2) {
+  if (process.argv.length !== 5) {
+    throw new Error("Invalid number of arguments. Please provide 3 side lengths.");
+  } else {
+    // eslint-disable-next-line no-console
+    console.log(Triangle.interpret(process.argv[2], process.argv[3], process.argv[4]));
+  }
+} else {
+  console.log("Please provide 3 side lengths");
+}
